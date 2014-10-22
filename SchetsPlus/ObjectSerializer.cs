49014231dbf86900cb76c211c938c731d5ca36a2
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2014-10-23 00:19:15 +0200

    serialization helper klasse

---
+++ SchetsPlus/ObjectSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Text;
+
+namespace SchetsEditor
+{
+    //Met dank aan: http://www.albahari.com/nutshell/ch15.aspx voor leerzame voorbeelden
+    public class ObjectSerializer
+    {
+        public static string Serialize<T>(T o)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(memoryStream, o);
+                return Encoding.UTF8.GetString(memoryStream.ToArray());
+            }
+        }
+
+        public static T Deserialize<T>(string s)
+        {
+            using (MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(s)))
+            {
+                XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(memoryStream, Encoding.UTF8, new XmlDictionaryReaderQuotas(), null);
+                DataContractSerializer serializer = new DataContractSerializer(typeof(T));
+                return (T)serializer.ReadObject(reader);
+            }
+        }
+    }
+}
--- SchetsPlus/SchetsObject.cs
+++ SchetsPlus/SchetsObject.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Runtime.Serialization;
 
 namespace SchetsEditor
 {
